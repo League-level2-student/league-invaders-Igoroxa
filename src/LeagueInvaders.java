c530333c4a84f7af6eabbe55430774ce11642cea
league <league@imac-34.attlocal.net>
2021-09-30 16:54:50 -0700

    Step 3

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,25 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+public static void main(String[] args) {
+	LeagueInvaders invaders = new LeagueInvaders();
+	invaders.setup();
+	
+	
+}
+JFrame frame;
+public static final int WIDTH = 500;
+public static final int HEIGHT= 800;
+GamePanel gp;
+LeagueInvaders(){
+	frame = new JFrame();
+	gp = new GamePanel();
+}
+void setup() {
 
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.add(gp);
+	frame.setSize(WIDTH, HEIGHT);
+}
 }
